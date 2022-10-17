1843f4f9fcf1401d820820e993092a44250e406e
elenazmeeva <biertli@bk.ru>
2022-10-17 23:10:00 +0300

    Initial commit

---
+++ src/main/java/com/example/courseWork3/CourseWork3Application.java
@@ -0,0 +1,16 @@
+package com.example.courseWork3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+
+public class CourseWork3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork3Application.class, args);
+	}
+
+}
